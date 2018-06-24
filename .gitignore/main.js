9add3c88526a0ceff89a23110566cdfc0f31f952
MineSeisme <36928520+MineSeisme@users.noreply.github.com>
2018-06-24 19:10:57 +0200

    Create main.js

---
+++ .gitignore/main.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+
+var bot = new Discord.Client();
+var prefix = ("*")
+
+bot.on('ready', () => {
+    bot.user.setPresence({ game: { name: '*help', type: 0}});
+    console.log("Le bot a démarré !");
+});
+
+bot.login('NDYwNDU2MzMzMTYyODQwMDc0.DhFBAA.mf257Z0uZAmaQX5TGs7fpsNkJjk');
+
+bot.on('message', message => {
+    if (message.content === "*ping"){
+        message.reply("pong");
+        console.log('La commande Ping a été demandée .');
+    }
+
+bot.on('message', message => {
+    if (message.content === "*version"){
+        message.reply("ArcaBot est en version 1.2 .");
+        console.log('La commande Version a été demandée');
+    }
+
+    if (message.content === (prefix + "help")){
+        var help_embed = new Discord.RichEmbed()
+        .setColor('#0FB09B')
+        .addField("Commandes d'ArcaBot :", "  *help : Affiche les commandes d'ArcaBot")
+        .addField("Autres commandes :", "  *version : Affiche la version d'ArcaBot")
+    message.channel.sendEmbed(help_embed);
+       // message.channel.sendMessage("Voici les commandes de ArcaBot :\n *help pour afficher les commandes");
+        console.log("La commande Help à été demandée !");
+    }
+    
+});})
