1d92991912706244e49c7a4091958544c65fb915
MineSeisme <36928520+MineSeisme@users.noreply.github.com>
2018-08-18 12:53:08 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -35,7 +35,7 @@ function play(connection, message) {
 
 
 client.on("guildMemberAdd", member => {
-  member.guild.channels.find("name", "💬discussion").send(`Salut ${member}, Bienvenue sur **Arcadia** !🎈🎉👍`)
+  member.guild.channels.find("name", "💬discussion").send(`Salut ${member}, Bienvenue sur **Arcadia** !🎈🎉👍 \nTu est actuellement le ${client.guild.users.size} ème membre du serveur ;)`)
 })
 
 client.on("guildMemberAdd", member => {
